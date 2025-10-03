43493452eb25e4ca534135b9476e4e5c37c11c7d
Yassine-Moussa <yassine.moussa@edu.esiee.fr>
2025-10-03 09:25:28 +0000

    j'ai eu un problème avec la somme

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-#### Imports et définition des variables globales
-
+"""Imports et définition des variables globales"""
 FILENAME = "listes.csv"
 
 #### Fonctions secondaires
 
-def read_data(filename):
+def read_data(l):
     """retourne le contenu du fichier <filename>
 
     Args:
@@ -14,39 +13,54 @@ def read_data(filename):
         list: le contenu du fichier (1 list par ligne)
     """
     l = []
+    with open("listes.csv","r",encoding="utf-8") as f:
+        for line in f:
+            numbers=line.split(r"[;, \t]+"),line.strip()
+            row=[str(i) for i in numbers]
+            l.append(row)
     return l
 
 def get_list_k(data, k):
-    l = []
+    """cette fonction nous donne le k-ième élément de la liste"""
+    l=[] #ajout d'une liste vide
+    l.append(data[k])
     return l
 
 def get_first(l):
-    return None
+    """cette fonction nous affiche le premier élément de la liste """
+    return l[0]
 
 def get_last(l):
-    return None
+    """cette fonction affiche le dernier élément de la liste"""
+    return l[-1]
 
 def get_max(l):
-    return None
+    """cette fonction affiche le plus grand élément de la liste"""
+    return max(l)
 
 def get_min(l):
-    return None
+    """cette fonction affiche le plus petit élément de la liste"""
+    return min(l)
 
 def get_sum(l):
-    return None
-
+    """cette fonction affiche la somme des élements de la liste"""
+    return sum(l)
 
 #### Fonction principale
 
 
 def main():
-    pass
-    # data = read_data(FILENAME)
-    # for i, l in enumerate(data):
-    #     print(i, l)
-    # k = 37
-    # print(k, get_list_k(data, 37))
-
+    """afficher les résultats souhaités"""
+    data = read_data(FILENAME)
+    for i, l in enumerate(data):
+        print(i, l)
+        k = 37
+    print(k, get_list_k(data, 37))
+    print("first:", get_first(data))
+    print("last:", get_last(data))
+    print("max:", get_max(data))
+    print("min:", get_min(data))
+    #print("sum:", get_sum(data))
 
 if __name__ == "__main__":
     main()
